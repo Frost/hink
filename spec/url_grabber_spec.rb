d88629e2c980ede5beab0f90d20c8dc7163a06d4
Victor Koronen <koronen@kth.se>
2011-07-19 14:34:15 +0200

    Add tests for sanitize_title

--- spec/url_grabber_spec.rb
+++ spec/url_grabber_spec.rb
@@ -2,14 +2,32 @@ require 'url-grabber'
 
 describe UrlGrabber do
   describe "extract_urls" do
-    nil.should be_nil
+  end
+
+  describe "sanitize_title" do
+    it "should not mess with sane titles" do
+      UrlGrabber.sanitize_title("").should == ""
+      UrlGrabber.sanitize_title("ab").should == "ab"
+    end
+
+    it "should handle linebreaks" do
+      UrlGrabber.sanitize_title("a\nb").should == "a b"
+      UrlGrabber.sanitize_title(" a\nb ").should == "a b"
+      UrlGrabber.sanitize_title(" a\n\n\nb ").should == "a   b"
+      UrlGrabber.sanitize_title("\nab\n").should == "ab"
+      UrlGrabber.sanitize_title("\n\n\nab\n\n\n").should == "ab"
+    end
+
+    it "should handle whitespace" do
+      UrlGrabber.sanitize_title("a b").should == "a b"
+      UrlGrabber.sanitize_title(" a b ").should == "a b"
+      UrlGrabber.sanitize_title("   a   b   ").should == "a   b"
+    end
   end
 
   describe "extract_title" do
-    nil.should be_nil
   end
 
   describe "bitlyfy" do
-    nil.should be_nil
   end
 end
