883c1e54e8a1b858d71db0961122ca483b87530a
Victor Koronen <koronen@kth.se>
2011-07-19 13:59:18 +0200

    Add rspec test stubs

---
+++ spec/url_grabber_spec.rb
@@ -0,0 +1,15 @@
+require 'url-grabber'
+
+describe UrlGrabber do
+  describe "extract_urls" do
+    nil.should be_nil
+  end
+
+  describe "extract_title" do
+    nil.should be_nil
+  end
+
+  describe "bitlyfy" do
+    nil.should be_nil
+  end
+end
