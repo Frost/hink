5b9eea8e3d9dbd786f75b0d619a8e44ab39a93ac
Victor Koronen <koronen@kth.se>
2011-07-20 15:15:41 +0200

    Add implementation of basic GitHub functionality

--- lib/github.rb
+++ lib/github.rb
@@ -10,5 +10,22 @@ class GitHub
 
   def execute(m)
     bot.logger.debug("received github referrence(s): #{m.message}")
+
+    GitHub.extract_references(m.message).each do |reference|
+      url = GitHub.reference_url(reference)
+      m.reply("#{m.user.nick}: #{url}")
+    end
+  end
+
+  def self.extract_references(text)
+    references = text.scan(/gh:([^\/ ]+)(?:\/([^# ]+)(?:\#(\d+))?)?/)
+    references.map{|r| {:account => r[0], :repo => r[1], :issue => (r[2].to_i if r[2])}}
+  end
+
+  def self.reference_url(reference)
+    url = "https://github.com/#{reference[:account]}"
+    url += "/#{reference[:repo]}" if reference[:repo]
+    url += "/issues/#{reference[:issue]}" if reference[:issue]
+    url
   end
 end
