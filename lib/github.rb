c751fae46784044ebb43566465da7bd83c73e390
Victor Koronen <koronen@kth.se>
2011-07-20 22:03:54 +0200

    Add implementation of default values

--- lib/github.rb
+++ lib/github.rb
@@ -12,14 +12,24 @@ class GitHub
     bot.logger.debug("received github referrence(s): #{m.message}")
 
     GitHub.extract_references(m.message).each do |reference|
+      reference = GitHub.merge_reference_with_default(reference, m.channel)
       url = GitHub.reference_url(reference)
       m.reply("#{m.user.nick}: #{url}")
     end
   end
 
   def self.extract_references(text)
-    references = text.scan(/gh:([^\/ ]+)(?:\/([^# ]+)(?:\#(\d+))?)?/)
+    references = text.scan(/gh:(?:(?!#)([^\/# ]+))?(?:\/([^# ]+))?(?:\#(\d+))?/)
     references.map{|r| {:account => r[0], :repo => r[1], :issue => (r[2].to_i if r[2])}}
+  end
+
+  def self.merge_reference_with_default(reference, channel)
+    $config[:github] ||= {}
+    $config[:github][channel.to_sym] ||= {}
+
+    default = {:account => nil, :repo => nil, :issue => nil}.merge($config[:github][channel.to_sym])
+
+    reference = default.merge(reference.select{|k,v| !v.nil?})
   end
 
   def self.reference_url(reference)
