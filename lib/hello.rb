ec3392df46ece0fae925612baec119eba169dc4c
Martin Frost <frost@ceri.se>
2011-09-19 17:44:52 +0200

    !hello now hilights whoever said '!hello'

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,10 @@
 class Hello
   include Cinch::Plugin
 
-  match 'hello'
+  match /hello/i
   react_on :channel
 
   def execute(m)
-    m.reply("hello")
+    m.reply("hello, #{m.user}!")
   end
 end
