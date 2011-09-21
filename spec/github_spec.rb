77e28426a91c8f21541077bcbccdb25f7451d05e
David Flemström <david.flemstrom@gmail.com>
2011-09-22 01:27:10 +0200

    Fix BD spec unit tests to include commit handling.

--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -3,28 +3,28 @@ require 'github'
 describe GitHub do
   describe "recognize github references" do
     it "should handle :account references" do
-      GitHub.extract_references("gh:Frost").should == [{:account => "Frost", :repo => nil, :issue => nil}]
-      GitHub.extract_references("look at gh:Frost").should == [{:account => "Frost", :repo => nil, :issue => nil}]
-      GitHub.extract_references("gh:Frost is a github account").should == [{:account => "Frost", :repo => nil, :issue => nil}]
+      GitHub.extract_references("gh:Frost").should == [{:account => "Frost", :repo => nil, :issue => nil, :commit => nil}]
+      GitHub.extract_references("look at gh:Frost").should == [{:account => "Frost", :repo => nil, :issue => nil, :commit => nil}]
+      GitHub.extract_references("gh:Frost is a github account").should == [{:account => "Frost", :repo => nil, :issue => nil, :commit => nil}]
     end
 
     it "should handle :account/:repo references" do
-      GitHub.extract_references("gh:Frost/url-grabber").should == [{:account => "Frost", :repo => "url-grabber", :issue => nil}]
-      GitHub.extract_references("look at gh:Frost/url-grabber").should == [{:account => "Frost", :repo => "url-grabber", :issue => nil}]
-      GitHub.extract_references("gh:Frost/url-grabber is a github repo").should == [{:account => "Frost", :repo => "url-grabber", :issue => nil}]
+      GitHub.extract_references("gh:Frost/url-grabber").should == [{:account => "Frost", :repo => "url-grabber", :issue => nil, :commit => nil}]
+      GitHub.extract_references("look at gh:Frost/url-grabber").should == [{:account => "Frost", :repo => "url-grabber", :issue => nil, :commit => nil}]
+      GitHub.extract_references("gh:Frost/url-grabber is a github repo").should == [{:account => "Frost", :repo => "url-grabber", :issue => nil, :commit => nil}]
     end
 
     it "should handle :account/:repo#:issue references" do
-      GitHub.extract_references("gh:Frost/url-grabber#1").should == [{:account => "Frost", :repo => "url-grabber", :issue => 1}]
-      GitHub.extract_references("look at gh:Frost/url-grabber#1").should == [{:account => "Frost", :repo => "url-grabber", :issue => 1}]
-      GitHub.extract_references("gh:Frost/url-grabber#1 is a github issue").should == [{:account => "Frost", :repo => "url-grabber", :issue => 1}]
+      GitHub.extract_references("gh:Frost/url-grabber#1").should == [{:account => "Frost", :repo => "url-grabber", :issue => 1, :commit => nil}]
+      GitHub.extract_references("look at gh:Frost/url-grabber#1").should == [{:account => "Frost", :repo => "url-grabber", :issue => 1, :commit => nil}]
+      GitHub.extract_references("gh:Frost/url-grabber#1 is a github issue").should == [{:account => "Frost", :repo => "url-grabber", :issue => 1, :commit => nil}]
     end
 
     it "should handle multiple references" do
       GitHub.extract_references("gh:Frost gh:Frost/url-grabber gh:Frost/url-grabber#1").should == [
-        {:account => "Frost", :repo => nil, :issue => nil},
-        {:account => "Frost", :repo => "url-grabber", :issue => nil},
-        {:account => "Frost", :repo => "url-grabber", :issue => 1}
+        {:account => "Frost", :repo => nil, :issue => nil, :commit => nil},
+        {:account => "Frost", :repo => "url-grabber", :issue => nil, :commit => nil},
+        {:account => "Frost", :repo => "url-grabber", :issue => 1, :commit => nil}
       ]
     end
   end
@@ -33,29 +33,29 @@ describe GitHub do
     before do
       $config = {}
       $config[:github] = {}
-      $config[:github][:"#withdefault"] = {:account => "Frost", :repo => "url-grabber", :issue => 1}
+      $config[:github][:"#withdefault"] = {:account => "Frost", :repo => "url-grabber", :issue => 1, :commit => nil}
     end
 
     it "should handle empty reference" do
-      GitHub.merge_reference_with_default({:account => nil, :repo => nil, :issue => nil}, "#withdefault").should == {:account => "Frost", :repo => "url-grabber", :issue => 1}
-      GitHub.merge_reference_with_default({:account => nil, :repo => nil, :issue => nil}, "#withoutdefault").should == {:account => nil, :repo => nil, :issue => nil}
+      GitHub.merge_reference_with_default({:account => nil, :repo => nil, :issue => nil}, "#withdefault").should == {:account => "Frost", :repo => "url-grabber", :issue => 1, :commit => nil}
+      GitHub.merge_reference_with_default({:account => nil, :repo => nil, :issue => nil}, "#withoutdefault").should == {:account => nil, :repo => nil, :issue => nil, :commit => nil}
     end
 
     it "should handle :account references" do
-      GitHub.merge_reference_with_default({:account => "Koronen", :repo => nil, :issue => nil}, "#withdefault").should == {:account => "Koronen", :repo => nil, :issue => nil}
-      GitHub.merge_reference_with_default({:account => "Koronen", :repo => nil, :issue => nil}, "#withoutdefault").should == {:account => "Koronen", :repo => nil, :issue => nil}
+      GitHub.merge_reference_with_default({:account => "Koronen", :repo => nil, :issue => nil}, "#withdefault").should == {:account => "Koronen", :repo => nil, :issue => nil, :commit => nil}
+      GitHub.merge_reference_with_default({:account => "Koronen", :repo => nil, :issue => nil}, "#withoutdefault").should == {:account => "Koronen", :repo => nil, :issue => nil, :commit => nil}
     end
 
     it "should handle (:account)/:repo references" do
-      GitHub.merge_reference_with_default({:account => nil, :repo => "spargris", :issue => nil}, "#withdefault").should == {:account => "Frost", :repo => "spargris", :issue => nil}
-      GitHub.merge_reference_with_default({:account => "Koronen", :repo => "spargris", :issue => nil}, "#withdefault").should == {:account => "Koronen", :repo => "spargris", :issue => nil}
-      GitHub.merge_reference_with_default({:account => "Koronen", :repo => "spargris", :issue => nil}, "#withoutdefault").should == {:account => "Koronen", :repo => "spargris", :issue => nil}
+      GitHub.merge_reference_with_default({:account => nil, :repo => "spargris", :issue => nil}, "#withdefault").should == {:account => "Frost", :repo => "spargris", :issue => nil, :commit => nil}
+      GitHub.merge_reference_with_default({:account => "Koronen", :repo => "spargris", :issue => nil}, "#withdefault").should == {:account => "Koronen", :repo => "spargris", :issue => nil, :commit => nil}
+      GitHub.merge_reference_with_default({:account => "Koronen", :repo => "spargris", :issue => nil}, "#withoutdefault").should == {:account => "Koronen", :repo => "spargris", :issue => nil, :commit => nil}
     end
 
     it "should handle (:account)(/:repo)#:issue references" do
-      GitHub.merge_reference_with_default({:account => nil, :repo => nil, :issue => 2}, "#withdefault").should == {:account => "Frost", :repo => "url-grabber", :issue => 2}
-      GitHub.merge_reference_with_default({:account => nil, :repo => "spargris", :issue => 2}, "#withdefault").should == {:account => "Frost", :repo => "spargris", :issue => 2}
-      GitHub.merge_reference_with_default({:account => "Koronen", :repo => "spargris", :issue => 2}, "#withoutdefault").should == {:account => "Koronen", :repo => "spargris", :issue => 2}
+      GitHub.merge_reference_with_default({:account => nil, :repo => nil, :issue => 2}, "#withdefault").should == {:account => "Frost", :repo => "url-grabber", :issue => 2, :commit => nil}
+      GitHub.merge_reference_with_default({:account => nil, :repo => "spargris", :issue => 2}, "#withdefault").should == {:account => "Frost", :repo => "spargris", :issue => 2, :commit => nil}
+      GitHub.merge_reference_with_default({:account => "Koronen", :repo => "spargris", :issue => 2}, "#withoutdefault").should == {:account => "Koronen", :repo => "spargris", :issue => 2, :commit => nil}
     end
   end
 
