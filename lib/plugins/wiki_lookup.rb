69acfb8f42e8ded910292294d8be3897276ce8be
Martin Frost <frost@ceri.se>
2012-09-27 17:41:04 +0200

    wiki lookup now handles multiple links

--- lib/plugins/wiki_lookup.rb
+++ lib/plugins/wiki_lookup.rb
@@ -4,17 +4,39 @@ require 'mechanize'
 class WikiLookup
   include Cinch::Plugin
 
+  LINK_REGEX = /\[\[([^\]]+)\]\]/
+
   set(prefix: '')
-  match /\[\[(.*)\]\]/, :react_on => :channel
+  match LINK_REGEX, :react_on => :channel
 
   def execute(m, term)
-    url = "#{Hink.config[:wiki_lookup][:url]}#{underscore term}"
-    agent = Mechanize.new
-    page = agent.get(url)
-    page_title = Nokogiri::HTML(page.body).at_css('title').content
-    
-    if page_title !~ /^Search results for .*/
-      m.reply("[[#{term}]]: #{url}")
+    result = self.class.extract_urls(m)
+    result.map do |match, title|
+      m.reply("[[#{match}]]: #{title}")
     end
   end
+
+  def self.extract_urls(message)
+    matches = message.message.scan(LINK_REGEX)
+
+    result = {}
+
+    matches.flatten.map do |match|
+      begin
+        url = "#{Hink.config[:wiki_lookup][:url]}#{underscore match}"
+        agent = Mechanize.new
+        page = agent.get(url)
+        page_title = Nokogiri::HTML(page.body).at_css('title').content
+
+        if page_title !~ /^Search results for .*/
+          result[match] = url
+        end
+      rescue Mechanize::ResponseCodeError => e
+        puts e.inspect
+      end
+    end
+
+    return result
+  end
+
 end
--- spec/lib/plugins/wiki_lookup_spec.rb
+++ spec/lib/plugins/wiki_lookup_spec.rb
@@ -2,5 +2,29 @@ require 'spec_helper'
 require 'plugins/wiki_lookup'
 
 describe WikiLookup do
-  it "needs some proper tests"
+  describe "LINK_REGEX" do
+    it "catches simple links" do
+      result = "[[simple]]".scan(WikiLookup::LINK_REGEX)
+
+      matches = result.map(&:first)
+
+      matches.should == ["simple"]
+    end
+
+    it "catches links with multiple words" do
+      result = "[[one two three]]".scan(WikiLookup::LINK_REGEX)
+
+      matches = result.map(&:first)
+
+      matches.should == ["one two three"]
+    end
+
+    it "catches a line with double links" do
+      result = "[[one]] and [[two]]".scan(WikiLookup::LINK_REGEX)
+
+      matches = result.map(&:first)
+
+      matches.should == ["one", "two"]
+    end
+  end
 end
