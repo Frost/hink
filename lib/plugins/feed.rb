b039fd647ed5f8448e5ca41e5aab577691d5782a
Jesper Bränn <brann.jesper@gmail.com>
2012-12-12 00:51:58 +0100

    Change @last_update into a hash and shorten the code a bit

--- lib/plugins/feed.rb
+++ lib/plugins/feed.rb
@@ -8,15 +8,15 @@ class Feed
   include Cinch::Plugin
 
   timer ::Hink.config[:feed][:interval].to_i*60, method: :check_news
-  @last_update = []
+  @last_update = {}
 
   def check_news
-    Hink.bot.logger.debug("checking news")
+#    Hink.bot.logger.debug("checking news")
     uris = Hink.config[:feed][:uris]
     news = []
 
-    uris.each_with_index do |uri, index|
-      news << self.class.check_news(uri, index)
+    uris.each_with_index do |uri|
+      news << self.class.check_news(uri)
     end
 
     news.flatten.each do |item|
@@ -26,16 +26,14 @@ class Feed
     end
   end
 
-  def self.check_news(uri, index)
+  def self.check_news(uri)
     agent = Mechanize.new
     feed = agent.get(uri).body
     rss = RSS::Parser.parse(feed)
-    @last_update[index] = Time.now.utc if @last_update[index].nil?
-    
-    items = rss.items.select do |i|
-      i.date.utc > @last_update[index]
-    end
-    @last_update[index] = rss.items.first.date.utc
+    @last_update[uri] = Time.now.utc if @last_update[uri].nil?
+
+    items = rss.items.select {|i| i.date.utc > @last_update[uri] }
+    @last_update[uri] = rss.items.first.date.utc
     
     output = items.collect do |item|
       item = Formatters::Feed.new(item.to_s, Hink.config[:feed][:template])
