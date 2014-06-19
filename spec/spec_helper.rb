c9ef83e452e1803583c78f1917bd41d2b762a97b
Martin Frost <frost@ceri.se>
2014-06-19 13:16:23 +0200

    setup codeclimate test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,10 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |c|
   c.fail_fast = false
+
+  # whitelist codeclimate.com so test coverage can be reported
+  c.after(:suite) do
+    WebMock.disable_net_connect!(:allow => 'codeclimate.com')
+  end
 end
+
