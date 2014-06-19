93c8e156a14e72e0feaf0b92d029219e715fa791
Martin Frost <frost@ceri.se>
2014-06-19 13:06:57 +0200

    restructured wiki_lookup plugin and added tests for it

--- lib/plugins/wiki_lookup.rb
+++ lib/plugins/wiki_lookup.rb
@@ -1,42 +1,60 @@
 require 'cinch'
 require 'mechanize'
+require 'grabber_helpers'
 
 class WikiLookup
   include Cinch::Plugin
+  include GrabberHelpers
 
   LINK_REGEX = /\[\[([^\]]+)\]\]/
 
-  set(prefix: '')
-  match LINK_REGEX, :react_on => :channel
+  set(
+    prefix: '',
+    :react_on => :channel
+  )
+  match LINK_REGEX
 
   def execute(m, term)
-    result = self.class.extract_urls(m)
+    result = extract_urls(m)
     result.map do |match, title|
       m.reply("[[#{match}]]: #{title}")
     end
   end
 
-  def self.extract_urls(message)
+  def extract_urls(message)
     matches = message.message.scan(LINK_REGEX)
 
     result = {}
 
     matches.flatten.map do |match|
-      begin
-        url = "#{Hink.config[:wiki_lookup][:url]}#{underscore match}"
-        agent = Mechanize.new
-        page = agent.get(url)
-        page_title = Nokogiri::HTML(page.body).at_css('title').content
-
-        if page_title !~ /^Search results for .*/
-          result[match] = url
-        end
-      rescue Mechanize::ResponseCodeError => e
-        puts e.inspect
+      if url = extract_url(match)
+        result[match] = url
       end
     end
 
     return result
   end
 
+  def extract_url(match)
+    url = url(match)
+    page = fetch_page(url)
+    return url if page && title(page) !~ /^Search results for .*/
+  end
+
+  def fetch_page(url)
+    begin
+      return Mechanize.new.get(url)
+    rescue Mechanize::ResponseCodeError => e
+      bot.loggers.debug e.inspect
+      return nil
+    end
+  end
+
+  def url(match)
+    Hink.config[:wiki_lookup][:url] + underscore(match)
+  end
+
+  def title(page)
+    Nokogiri::HTML(page.body).at_css('title').content
+  end
 end
--- spec/lib/plugins/wiki_lookup_spec.rb
+++ spec/lib/plugins/wiki_lookup_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 require 'plugins/wiki_lookup'
+require 'ostruct'
 
 describe WikiLookup do
+  let(:bot) { Cinch::Bot.new }
+  before(:all) do
+    bot.loggers.level = :fatal
+    Hink.setup(bot)
+  end
+
   describe "LINK_REGEX" do
     it "catches simple links" do
       result = "[[simple]]".scan(WikiLookup::LINK_REGEX)
@@ -25,6 +32,66 @@ describe WikiLookup do
       matches = result.map(&:first)
 
       matches.should == ["one", "two"]
+    end
+  end
+
+  describe "extract_urls" do
+    subject { WikiLookup.new(bot) }
+    let(:message) { OpenStruct.new }
+
+    it "returns the url for the wiki page if it exists" do
+      stub_request(:get, "#{Hink.config[:wiki_lookup][:url]}foobar").to_return(
+        status: 200,
+        body: "<html><head><title>Foobar</title></head></html>"
+      )
+
+      message.message = "[[Foobar]]"
+
+      result = subject.extract_urls(message)
+      result.should == {
+        "Foobar" => "#{Hink.config[:wiki_lookup][:url]}foobar"
+      }
+
+    end
+
+    it "can extract multiple urls" do
+      stub_request(:get, "#{Hink.config[:wiki_lookup][:url]}foo").to_return(
+        status: 200,
+        body: "<html><head><title>Foo</title></head></html>"
+      )
+      stub_request(:get, "#{Hink.config[:wiki_lookup][:url]}bar").to_return(
+        status: 200,
+        body: "<html><head><title>Bar</title></head></html>"
+      )
+
+      message.message = "[[Foo]] [[Bar]]"
+
+      result = subject.extract_urls(message)
+      result.should == {
+        "Foo" => "#{Hink.config[:wiki_lookup][:url]}foo",
+        "Bar" => "#{Hink.config[:wiki_lookup][:url]}bar"
+      }
+    end
+
+    it "doesn't return anything for non-existing pages" do
+      stub_request(:get, "#{Hink.config[:wiki_lookup][:url]}foo").to_return(
+        status: 200,
+        body: "<html><head><title>Search results for foo</title></head></html>"
+      )
+
+      message.message = "[[Foo]]"
+      result = subject.extract_urls(message)
+      result.should == {}
+    end
+
+    it "returns nothing if a request fails" do
+      stub_request(:get, "#{Hink.config[:wiki_lookup][:url]}foo").to_return(
+        status: 404,
+        body: "<html><head><title>Search results for foo</title></head></html>"
+      )
+      message.message = "[[Foo]]"
+      result = subject.extract_urls(message)
+      result.should == {}
     end
   end
 end
